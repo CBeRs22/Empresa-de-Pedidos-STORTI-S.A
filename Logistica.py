c75f1d0ab93cacb569be8ffbce05cabdcaadd8f1
Leonel Cabral <69341023+CBeRs22@users.noreply.github.com>
2023-10-26 05:35:14 -0300

    Update Logistica.py

--- Logistica.py
+++ Logistica.py
@@ -1,180 +1,178 @@
-import tkinter as tk
+import tkinter
 from tkinter import ttk
 import sqlite3
 
-iniciar_aplicacion()
+conexion = sqlite3.connect('Logistica.db')
+datos = conexion.cursor()
 
-# Conectar a la base de datos
-conn = sqlite3.connect('Logistica.db')
-cursor = conn.cursor()
+# Crea la ventana.
 
-# Crear una ventana
-app = tk.Tk()
-app.title("Gestión de Pedidos")
-app.geometry('540x380')
+ventana = tkinter.Tk()
+ventana.title("Gestión de Pedidos") # Nombre de la ventana.
+ventana.geometry('640x480') # Tamaño de la ventana.
 
-# Crear pestañas
-notebook = ttk.Notebook(app)
-tab_clientes = ttk.Frame(notebook)
-tab_productos = ttk.Frame(notebook)
-tab_pedidos = ttk.Frame(notebook)
-notebook.add(tab_clientes, text="Clientes")
-notebook.add(tab_productos, text="Productos")
-notebook.add(tab_pedidos, text="Pedidos")
-notebook.pack()
+# Crea las pestañas.
 
-# Función para buscar un cliente por ID y mostrar su información
-def buscar_cliente():
-    id_cliente = entry_id_cliente.get()
-    cursor.execute("SELECT * FROM Clientes WHERE IDCliente=?", (id_cliente,))
-    cliente = cursor.fetchone()
+pestania = ttk.Notebook(ventana)
+pestaclient = ttk.Frame(pestania, width = 640, height = 480)
+pestaprodu = ttk.Frame(pestania, width = 640, height = 480)
+pestapedido = ttk.Frame(pestania, width = 640, height = 480)
+pestania.add(pestaclient, text = "Clientes")
+pestania.add(pestaprodu, text = "Productos")
+pestania.add(pestapedido, text = "Pedidos")
+pestania.pack()
+
+# Formulario para agregar clientes.
+
+etiqueta2 = tkinter.Label(pestaclient, text = "Nombre:")
+etiqueta2.grid(row = 0, column = 0)
+entrada2 = tkinter.Entry(pestaclient)
+entrada2.grid(row = 0, column = 1)
+etiqueta3 = tkinter.Label(pestaclient, text = "Dirección:")
+etiqueta3.grid(row = 1, column = 0)
+entrada3 = tkinter.Entry(pestaclient)
+entrada3.grid(row = 1, column =1 )
+etiqueta4 = tkinter.Label(pestaclient, text = "Teléfono:")
+etiqueta4.grid(row = 2, column = 0)
+entrada4 = tkinter.Entry(pestaclient)
+entrada4.grid(row = 2, column = 1)
+etiqueta5 = tkinter.Label(pestaclient, text = "Email:")
+etiqueta5.grid(row = 3, column = 0)
+entrada5 = tkinter.Entry(pestaclient)
+entrada5.grid(row = 3, column = 1)
+
+def agregarclient():
+    nombre = entrada2.get()
+    direccion = entrada3.get()
+    telefono = entrada4.get()
+    email = entrada5.get()
+    datos.execute("INSERT INTO Clientes (NOMBRE, DIRECCIÓN, TELEFONO, EMAIL) VALUES (?, ?, ?, ?)", (nombre, direccion, telefono, email))
+    conexion.commit()
+    entrada2.delete(0, tkinter.END)
+    entrada3.delete(0, tkinter.END)
+    entrada4.delete(0, tkinter.END)
+    entrada5.delete(0, tkinter.END)
+
+boton2 = tkinter.Button(pestaclient, text = "Agregar Cliente", command = agregarclient)
+boton2.grid(row = 4, columnspan = 2)
+
+# Formulario para agregar productos.
+
+etiqueta6 = tkinter.Label(pestaprodu, text = "Nombre del Producto:")
+etiqueta6.grid(row = 0, column = 0)
+entrada6 = tkinter.Entry(pestaprodu)
+entrada6.grid(row = 0, column = 1)
+etiqueta7 = tkinter.Label(pestaprodu, text = "Descripción:")
+etiqueta7.grid(row = 1, column = 0)
+entrada7 = tkinter.Entry(pestaprodu)
+entrada7.grid(row = 1, column = 1)
+etiqueta8 = tkinter.Label(pestaprodu, text = "Precio Unitario:")
+etiqueta8.grid(row = 2, column = 0)
+entrada8 = tkinter.Entry(pestaprodu)
+entrada8.grid(row = 2, column = 1)
+
+def agregarprodu():
+    nombre = entrada6.get()
+    descripcion = entrada7.get()
+    precio = entrada8.get()
+    datos.execute("INSERT INTO Productos (NOMBRE, DESCRIPCION, PRECIOUNITARIO) VALUES (?, ?, ?)", (nombre, descripcion, precio))
+    conexion.commit()
+    entrada6.delete(0, tkinter.END)
+    entrada7.delete(0, tkinter.END)
+    entrada8.delete(0, tkinter.END)
+
+boton3 = tkinter.Button(pestaprodu, text = "Agregar Producto", command = agregarprodu)
+boton3.grid(row = 3, columnspan = 2)
+
+# Formulario para buscar clientes.
+
+etiqueta0 = tkinter.Label(pestapedido, text = "ID del Cliente:")
+etiqueta0.grid(row = 0, column = 0)
+entrada0 = tkinter.Entry(pestapedido)
+entrada0.grid(row = 0, column = 1)
+resultado0 = tkinter.Label(pestapedido, text = "")
+resultado0.grid(row = 2, columnspan = 2)
+
+def buscarclientes():
+    infocli = entrada0.get()
+    datos.execute("SELECT * FROM Clientes WHERE IDCliente = ? ", (infocli))
+    cliente = datos.fetchone()   
     if cliente:
-        resultado_cliente.config(text=f"ID: {cliente[0]}\nNombre: {cliente[1]}\nDirección: {cliente[2]}\nTeléfono: {cliente[3]}\nEmail: {cliente[4]}\nFecha de Registro: {cliente[5]}\nNotas: {cliente[6]}")
+        resultado0.config(text = f"ID: {cliente[0]}\nNombre: {cliente[1]}\nDirección: {cliente[2]}\nTeléfono: {cliente[3]}\nEmail: {cliente[4]}\nFecha de Registro: {cliente[5]}\nNotas: {cliente[6]}")
     else:
-        resultado_cliente.config(text="Cliente no encontrado.")
+        resultado0.config(text = "Cliente no encontrado.")
 
-# Función para buscar un producto por ID y mostrar su información
-def buscar_producto():
-    id_producto = entry_id_producto.get()
-    cursor.execute("SELECT * FROM Productos WHERE IDProducto=?", (id_producto,))
-    producto = cursor.fetchone()
+boton0 = tkinter.Button(pestapedido, text = "Buscar Cliente", command = buscarclientes)
+boton0.grid(row = 1, columnspan = 2)
+
+# Formulario para buscar productos.
+
+etiqueta9 = tkinter.Label(pestapedido, text = "ID del Producto:")
+etiqueta9.grid(row = 3, column = 0)
+entrada9 = tkinter.Entry(pestapedido)
+entrada9.grid(row = 3, column = 1)
+etiqueta10 = tkinter.Label(pestapedido, text = "")
+etiqueta10.grid(row = 5, columnspan = 2)
+
+def buscarprodu():
+    infoprodu = entrada9.get()
+    datos.execute("SELECT * FROM Productos WHERE IDProducto = ? ",(infoprodu))
+    producto = datos.fetchone()
     if producto:
-        resultado_producto.config(text=f"ID: {producto[0]}\nNombre: {producto[1]}\nDescripción: {producto[2]}\nPrecio Unitario: {producto[3]}\nStock Inicial: {producto[4]}\nStock Mínimo: {producto[5]}\nFecha de Creación: {producto[6]}")
+        etiqueta10.config(text = f"ID: {producto[0]}\nNombre: {producto[1]}\nDescripción: {producto[2]}\nPrecio Unitario: {producto[3]}\nStock Inicial: {producto[4]}\nStock Mínimo: {producto[5]}\nFecha de Creación: {producto[6]}")
     else:
-        resultado_producto.config(text="Producto no encontrado.")
+        etiqueta10.config(text = "Producto no encontrado.")
 
-# Función para crear un nuevo pedido
-def crear_pedido():
-    id_cliente = entry_id_cliente_pedido.get()
-    id_vendedor = entry_id_vendedor_pedido.get()
-    id_zona = entry_id_zona_pedido.get()
-    estado_pedido = entry_estado_pedido.get()
-    notas = entry_notas_pedido.get()
-    cursor.execute("INSERT INTO Pedidos (FechaPedido, IDCliente, IDVendedor, IDZona, EstadoPedido, Notas) VALUES (datetime('now'), ?, ?, ?, ?, ?)", (id_cliente, id_vendedor, id_zona, estado_pedido, notas))
-    conn.commit()
-    entry_id_cliente_pedido.delete(0, tk.END)
-    entry_id_vendedor_pedido.delete(0, tk.END)
-    entry_id_zona_pedido.delete(0, tk.END)
-    entry_estado_pedido.delete(0, tk.END)
-    entry_notas_pedido.delete(0, tk.END)
-
-# Crear pestaña de Pedidos
-tab_pedidos = ttk.Frame(notebook)
-notebook.add(tab_pedidos, text="Pedidos")
-
-# Formulario para buscar clientes
-label_id_cliente = tk.Label(tab_pedidos, text="ID del Cliente:")
-entry_id_cliente = tk.Entry(tab_pedidos)
-btn_buscar_cliente = tk.Button(tab_pedidos, text="Buscar Cliente", command=buscar_cliente)
-resultado_cliente = tk.Label(tab_pedidos, text="")
-label_id_cliente.grid(row=0, column=0)
-entry_id_cliente.grid(row=0, column=1)
-btn_buscar_cliente.grid(row=1, columnspan=2)
-resultado_cliente.grid(row=2, columnspan=2)
-
-# Formulario para buscar productos
-label_id_producto = tk.Label(tab_pedidos, text="ID del Producto:")
-entry_id_producto = tk.Entry(tab_pedidos)
-btn_buscar_producto = tk.Button(tab_pedidos, text="Buscar Producto", command=buscar_producto)
-resultado_producto = tk.Label(tab_pedidos, text="")
-label_id_producto.grid(row=3, column=0)
-entry_id_producto.grid(row=3, column=1)
-btn_buscar_producto.grid(row=4, columnspan=2)
-resultado_producto.grid(row=5, columnspan=2)
+boton4 = tkinter.Button(pestapedido, text = "Buscar Producto", command = buscarprodu)
+boton4.grid(row = 4, columnspan = 2)
 
 # Formulario para crear un nuevo pedido
-label_id_cliente_pedido = tk.Label(tab_pedidos, text="ID del Cliente:")
-entry_id_cliente_pedido = tk.Entry(tab_pedidos)
-label_id_vendedor_pedido = tk.Label(tab_pedidos, text="ID del Vendedor:")
-entry_id_vendedor_pedido = tk.Entry(tab_pedidos)
-label_id_zona_pedido = tk.Label(tab_pedidos, text="ID de la Zona:")
-entry_id_zona_pedido = tk.Entry(tab_pedidos)
-label_estado_pedido = tk.Label(tab_pedidos, text="Estado del Pedido:")
-entry_estado_pedido = tk.Entry(tab_pedidos)
-label_notas_pedido = tk.Label(tab_pedidos, text="Notas:")
-entry_notas_pedido = tk.Entry(tab_pedidos)
-btn_crear_pedido = tk.Button(tab_pedidos, text="Crear Pedido", command=crear_pedido)
-label_id_cliente_pedido.grid(row=6, column=0)
-entry_id_cliente_pedido.grid(row=6, column=1)
-label_id_vendedor_pedido.grid(row=7, column=0)
-entry_id_vendedor_pedido.grid(row=7, column=1)
-label_id_zona_pedido.grid(row=8, column=0)
-entry_id_zona_pedido.grid(row=8, column=1)
-label_estado_pedido.grid(row=9, column=0)
-entry_estado_pedido.grid(row=9, column=1)
-label_notas_pedido.grid(row=10, column=0)
-entry_notas_pedido.grid(row=10, column=1)
-btn_crear_pedido.grid(row=11, columnspan=2)
 
+etiqueta11 = tkinter.Label(pestapedido, text = "ID del Cliente:")
+etiqueta11.grid(row = 6, column = 0)
+entrada10 = tkinter.Entry(pestapedido)
+entrada10.grid(row= 6 , column = 1)
+etiqueta12 = tkinter.Label(pestapedido, text = "ID del Vendedor:")
+etiqueta12.grid(row = 7, column = 0)
+entrada11 = tkinter.Entry(pestapedido)
+entrada11.grid(row = 7, column = 1)
+etiqueta13 = tkinter.Label(pestapedido, text = "ID de la Zona:")
+etiqueta13.grid(row = 8, column = 0)
+entrada12 = tkinter.Entry(pestapedido)
+entrada12.grid(row = 8, column = 1)
+etiqueta14 = tkinter.Label(pestapedido, text = "Estado del Pedido:")
+etiqueta14.grid(row = 9, column = 0)
+entrada13 = tkinter.Entry(pestapedido)
+entrada13.grid(row = 9, column = 1)
+etiqueta15 = tkinter.Label(pestapedido, text = "Notas:")
+etiqueta15.grid(row = 10, column = 0)
+entrada14 = tkinter.Entry(pestapedido)
+entrada14.grid(row = 10, column = 1)
 
-# Función para agregar un cliente a la base de datos
-def agregar_cliente():
-    nombre = entry_nombre.get()
-    direccion = entry_direccion.get()
-    telefono = entry_telefono.get()
-    email = entry_email.get()
-    cursor.execute("INSERT INTO Clientes (NombreCliente, DireccionCliente, TelefonoCliente, EmailCliente) VALUES (?, ?, ?, ?)", (nombre, direccion, telefono, email))
-    conn.commit()
-    entry_nombre.delete(0, tk.END)
-    entry_direccion.delete(0, tk.END)
-    entry_telefono.delete(0, tk.END)
-    entry_email.delete(0, tk.END)
+def crearpedido():
+    infocli = entrada10.get()
+    infovend = entrada11.get()
+    infozona = entrada12.get()
+    infopedi = entrada13.get()
+    notas = entrada14.get()
+    datos.execute("INSERT INTO Pedidos (FECHAS, IDCliente, IDVendedor, IDZona, ESTADOPEDIDO, NOTAS) VALUES (datetime('now'), ?, ?, ?, ?, ?)", (infocli, infovend, infozona, infopedi, notas))
+    conexion.commit()
+    entrada10.delete(0, tkinter.END)
+    entrada11.delete(0, tkinter.END)
+    entrada12.delete(0, tkinter.END)
+    entrada13.delete(0, tkinter.END)
+    entrada14.delete(0, tkinter.END)
 
-# Crear formulario para agregar clientes
-label_nombre = tk.Label(tab_clientes, text="Nombre:")
-entry_nombre = tk.Entry(tab_clientes)
-label_direccion = tk.Label(tab_clientes, text="Dirección:")
-entry_direccion = tk.Entry(tab_clientes)
-label_telefono = tk.Label(tab_clientes, text="Teléfono:")
-entry_telefono = tk.Entry(tab_clientes)
-label_email = tk.Label(tab_clientes, text="Email:")
-entry_email = tk.Entry(tab_clientes)
-btn_agregar_cliente = tk.Button(tab_clientes, text="Agregar Cliente", command=agregar_cliente)
+boton5 = tkinter.Button(pestapedido, text = "Crear Pedido", command = crearpedido)
+boton5.grid(row = 11, columnspan = 2)
 
-label_nombre.grid(row=0, column=0)
-entry_nombre.grid(row=0, column=1)
-label_direccion.grid(row=1, column=0)
-entry_direccion.grid(row=1, column=1)
-label_telefono.grid(row=2, column=0)
-entry_telefono.grid(row=2, column=1)
-label_email.grid(row=3, column=0)
-entry_email.grid(row=3, column=1)
-btn_agregar_cliente.grid(row=4, columnspan=2)
+# Función para cerrar la conexión con la base de datos y salir de la ventana.
 
-# Función para agregar un producto a la base de datos
-def agregar_producto():
-    nombre = entry_nombre_producto.get()
-    descripcion = entry_descripcion.get()
-    precio = entry_precio.get()
-    cursor.execute("INSERT INTO Productos (NombreProducto, Descripcion, PrecioUnitario) VALUES (?, ?, ?)", (nombre, descripcion, precio))
-    conn.commit()
-    entry_nombre_producto.delete(0, tk.END)
-    entry_descripcion.delete(0, tk.END)
-    entry_precio.delete(0, tk.END)
-
-# Crear formulario para agregar productos
-label_nombre_producto = tk.Label(tab_productos, text="Nombre del Producto:")
-entry_nombre_producto = tk.Entry(tab_productos)
-label_descripcion = tk.Label(tab_productos, text="Descripción:")
-entry_descripcion = tk.Entry(tab_productos)
-label_precio = tk.Label(tab_productos, text="Precio Unitario:")
-entry_precio = tk.Entry(tab_productos)
-btn_agregar_producto = tk.Button(tab_productos, text="Agregar Producto", command=agregar_producto)
-
-label_nombre_producto.grid(row=0, column=0)
-entry_nombre_producto.grid(row=0, column=1)
-label_descripcion.grid(row=1, column=0)
-entry_descripcion.grid(row=1, column=1)
-label_precio.grid(row=2, column=0)
-entry_precio.grid(row=2, column=1)
-btn_agregar_producto.grid(row=3, columnspan=2)
-
-# Función para cerrar la conexión con la base de datos y salir
 def salir():
-    conn.close()
-    app.destroy()
+    conexion.close()
+    ventana.destroy()
 
-btn_salir = tk.Button(app, text="Salir", command=salir)
-btn_salir.pack()
+boton1 = tkinter.Button(ventana, text = "Salir", command = salir)
+boton1.pack()
 
-app.mainloop()
+ventana.mainloop()
